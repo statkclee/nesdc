8aab192dc81479cd3e8a1d59794bb192e81def96
Kwangchun Lee <kwangchun.lee.7@gmail.com>
2024-05-27 13:54:10 +0900

    added quote

---
+++ quote.R
@@ -0,0 +1,19 @@
+# Quotes to Scrape 웹사이트에서 오늘의 명언
+library(rvest)
+library(tidyverse)
+
+url <- "https://quotes.toscrape.com"
+html <- read_html(url)
+
+row <- html |> html_elements(".quote")
+
+daily_quote <- row |> html_element(".text") |> html_text2() %>% .[1]
+
+daily_author <- row |> html_element(".author") |> html_text2() %>% .[1]
+
+quote_msg <- paste0(daily_author, ": ", daily_quote)
+
+ntfy::ntfy_send(quote_msg, topic = Sys.getenv("NTFY_QUOTE_TOPIC"))
+
+
+
